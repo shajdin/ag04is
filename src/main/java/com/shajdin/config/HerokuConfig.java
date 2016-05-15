ad91be3722cece6221d211f49b2a225ae823bcfa
shajdin <sergej.hajdin@yahoo.com>
2016-05-15 05:15:33 +0200

    heroku datasource config 2

--- src/main/java/com/shajdin/config/HerokuConfig.java
+++ src/main/java/com/shajdin/config/HerokuConfig.java
@@ -1,6 +1,9 @@
 package com.shajdin.config;
 
+import java.net.URI;
 import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
@@ -14,19 +17,51 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Profile("heroku")
 public class HerokuConfig {
 	
+//	@Bean
+//    public BasicDataSource dataSource() throws URISyntaxException {
+//        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+//        String username = System.getenv("JDBC_DATABASE_USERNAME");
+//        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+//
+//        BasicDataSource basicDataSource = new BasicDataSource();
+//        basicDataSource.setUrl(dbUrl);
+//        basicDataSource.setUsername(username);
+//        basicDataSource.setPassword(password);
+//
+//        return basicDataSource;
+//    }
+	
 	@Bean
-    public BasicDataSource dataSource() throws URISyntaxException {
-        String dbUrl = System.getenv("JDBC_DATABASE_URL");
-        String username = System.getenv("JDBC_DATABASE_USERNAME");
-        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+	public DataSource herokuDataSource() {
+		String databaseUrl = System.getenv("DATABASE_URL");
 
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+		URI dbUri;
+		try {
+			dbUri = new URI(databaseUrl);
+		} catch (URISyntaxException e) {
+			return null;
+		}
 
-        return basicDataSource;
-    }
+		String username = dbUri.getUserInfo().split(":")[0];
+		String password = dbUri.getUserInfo().split(":")[1];
+		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+		org.apache.tomcat.jdbc.pool.DataSource dataSource = new org.apache.tomcat.jdbc.pool.DataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(dbUrl);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		dataSource.setTestOnBorrow(true);
+		dataSource.setTestWhileIdle(true);
+		dataSource.setTestOnReturn(true);
+		dataSource.setMaxActive(10);
+		dataSource.setMaxIdle(5);
+		dataSource.setMinIdle(2);
+		dataSource.setInitialSize(5);
+		dataSource.setRemoveAbandoned(true);
+		dataSource.setValidationQuery("SELECT 1");
+		return dataSource;
+	}
 	
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws URISyntaxException{
@@ -41,7 +76,7 @@ public class HerokuConfig {
 		
 		LocalContainerEntityManagerFactoryBean emfb = 
 			new LocalContainerEntityManagerFactoryBean();
-		emfb.setDataSource(dataSource());
+		emfb.setDataSource(herokuDataSource());
 		emfb.setPackagesToScan("com.shajdin.model");
 //		emfb.setJpaProperties(props);
 		emfb.setJpaVendorAdapter(adapter);
